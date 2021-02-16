d4b852c1a2456c58dd5de8e4b69cc4499a4fe18a
Gustavo Fe <gugudeveloper@gmail.com>
2021-02-16 16:51:33 -0300

    Added boostrap theme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,13 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("bootstrap")
+import "../stylesheets/application";
+document.addEventListener("turbolinks:load", function() {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+    })
+})
+
 import 'app';
